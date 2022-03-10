202f82560b0ef58b41b19ca563ef3c9f01c7afec
Dmitry <nda2911@yandex.ru>
2022-03-10 23:14:21 +0300

    Disabled parsing commands in group chats

--- imports/Controller.js
+++ imports/Controller.js
@@ -45,50 +45,54 @@ function Controller(parameters) {
 
 Controller.prototype.processTelegramData = async function(data) {
   if (data.cmd) {
-    if (this.commands[data.cmd.cmd]) {
-      this.commands[data.cmd.cmd](data, this);
-    }
-    else {
-      this.telegram.sendUnknownCmd(data.chatId);
+    if (data.raw.chat.type != 'group') {
+      if (this.commands[data.cmd.cmd]) {
+        this.commands[data.cmd.cmd](data, this);
+      }
+      else {
+        this.telegram.sendUnknownCmd(data.chatId);
+      }
     }
   }
   else {
     if (data.raw.voice) {
-      if (data.raw.voice.duration > this.parameters.config.voice.max_duration) {
-        return;
-      }
+      if (!data.raw.from.is_bot) {
+        if (data.raw.voice.duration > this.parameters.config.voice.max_duration) {
+          return;
+        }
 
-      let link = await this.telegram.bot.getFileLink(data.raw.voice.file_id);
+        let link = await this.telegram.bot.getFileLink(data.raw.voice.file_id);
 
-      debugOut(`# Got file link ${link}`);
+        debugOut(`# Got file link ${link}`);
 
-      let file = await axios.get(link, {
-        responseType: 'arraybuffer'
-      });
+        let file = await axios.get(link, {
+          responseType: 'arraybuffer'
+        });
 
-      if (file.status != 200) {
-        debugOut(`# Failed to dowload file ${data.raw.voice.file_id}`);
+        if (file.status != 200) {
+          debugOut(`# Failed to dowload file ${data.raw.voice.file_id}`);
 
-        return;
-      }
+          return;
+        }
 
-      let filedata = file.data;
+        let filedata = file.data;
 
-      let text = await this.voice.request({
-        mime: data.raw.voice.mime_type,
-        data: filedata
-      });
+        let text = await this.voice.request({
+          mime: data.raw.voice.mime_type,
+          data: filedata
+        });
 
-      if (text) {
-        debugOut(`# Recognized message with dutaion ${data.raw.voice.duration} seconds`);
+        if (text) {
+          debugOut(`# Recognized message with dutaion ${data.raw.voice.duration} seconds`);
 
-        this.telegram.sendMessage(
-          data.chatId, 
-          text, 
-          { reply_to_message_id: data.messageId,
-            reply_markup: { remove_keyboard: true }, 
-            parse_mode: "HTML", 
-            disable_web_page_preview: true});
+          this.telegram.sendMessage(
+            data.chatId, 
+            text, 
+            { reply_to_message_id: data.messageId,
+              reply_markup: { remove_keyboard: true }, 
+              parse_mode: "HTML", 
+              disable_web_page_preview: true});
+        }
       }
     }
   }
